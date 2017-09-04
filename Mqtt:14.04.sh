2dcf1dad83cbd34db49e104836d73bd0ee93f4b7
Thanakorn   Kriangudom <month219@gmail.com>
2017-09-04 15:45:44 +0700

    Update Mqtt:14.04.sh

--- Mqtt:14.04.sh
+++ Mqtt:14.04.sh
@@ -1,11 +1,11 @@
 #Add the mosquitto repository by given below commands
 sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
-sudo apt-get update
+sudo apt-get update -y
 #Execute the given below command to install the Mosquitto broker package
-sudo apt-get install mosquitto
+sudo apt-get install mosquitto -y
 #Install Mosquitto developer libraries to develop MQTT clients
-sudo apt-get install libmosquitto-dev
+sudo apt-get install libmosquitto-dev -y
 #Execute the given below command to install Mosquitto client packages
-sudo apt-get install mosquitto-clients
+sudo apt-get install mosquitto-clients -y
 #Ensure that Mosquitto broker is running
 sudo service mosquitto status 
